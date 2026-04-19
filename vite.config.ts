9a79501f7a1dc7af02d4eacbb813d455039c8f11
kintwadi <joinemabi@gmail.com>
2026-04-19 21:08:47 +0200

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,11 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: '/sharehopes-r/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
-    base: './sharehopes-r/',
+  
     build: {
        outDir: 'dist',
        sourcemap: false,
