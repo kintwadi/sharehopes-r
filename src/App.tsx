9d10df128f6cc94ccb2a6b40d9a6e769c99c4058
kintwadi <joinemabi@gmail.com>
2026-04-20 22:20:50 +0200

    cname

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
@@ -9,8 +9,23 @@ import { PrivacyPage } from './pages/PrivacyPage';
 import { TermsPage } from './pages/TermsPage';
 import { CookiePage } from './pages/CookiePage';
 import { ScrollToTop } from './components/ScrollToTop';
+import { React,useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function App() {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const redirectPath = params.get('redirect');
+    if (redirectPath) {
+      // Clean up URL and navigate
+      window.history.replaceState(null, '', redirectPath);
+      navigate(redirectPath, { replace: true });
+    }
+  }, [location, navigate]);
   return (
     <BrowserRouter>
       <ScrollToTop />
